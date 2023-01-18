aa9dbcc7f4b4a76c32c06644474252548c496fc2
Geraldine <gerahernan.al@gmail.com>
2023-01-17 17:36:23 -0800

    feat: added postcss and autorefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 const { src, series, watch, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
-
-
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 
 function css(done) {
     // body omitted
     src('src/scss/app.scss')
         .pipe(sass())
+        .pipe(postcss( [autoprefixer()]))
         .pipe(dest('build/css'))
 
         done();
   }
+
+  function dev(){
+    watch('src/scss/app.scss', css)
+  }
   
   exports.css = css;
+  exports.dev = dev
